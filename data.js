a2d440dda1b8c0ff91207973acfb3c70ecc9b759
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-08-02 00:41:16 -0400

    init

---
+++ data.js
@@ -0,0 +1,139 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overall-view",
+      "name": "Overall View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.48684803407316934,
+        "pitch": 0.25201644362916653,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10017636672596097,
+          "pitch": 0.24468764393465392,
+          "rotation": 0,
+          "target": "1-eagle-b-pit"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.304698249656603,
+          "pitch": 0.09130759319277004,
+          "title": "Development Area",
+          "text": "Development yards for future pits."
+        },
+        {
+          "yaw": -2.561499065876177,
+          "pitch": 0.4087333233885211,
+          "title": "Durable Rock Fill - B",
+          "text": "Road is established to build DRF-B from the toe up."
+        },
+        {
+          "yaw": 0.6434425588579273,
+          "pitch": 0.1622109552632125,
+          "title": "Stockpile Area",
+          "text": "All mined coal is stockpiled in this area and trucks are loaded from these piles."
+        },
+        {
+          "yaw": 1.4867536698479809,
+          "pitch": 0.28965345117461183,
+          "title": "Durable Rock Fill - A",
+          "text": "Road is established to build DRF - A from the toe up. Approximately two terraces are nearly completed."
+        },
+        {
+          "yaw": 2.5588231500230076,
+          "pitch": 0.19941089606331275,
+          "title": "Sediment Channels",
+          "text": "Sediment channels are constructed around the outer edge of the mining area to control drainage."
+        }
+      ]
+    },
+    {
+      "id": "1-eagle-b-pit",
+      "name": "Eagle B Pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.41507102938335905,
+        "pitch": 0.2559652061389244,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.870147475513015,
+          "pitch": -0.22059929612214368,
+          "rotation": 0,
+          "target": "0-overall-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3786142778215984,
+          "pitch": 0.020135284881826365,
+          "title": "Stockpile Area",
+          "text": "All mined coal is stockpiled in this area and trucks are loaded from these piles."
+        },
+        {
+          "yaw": -0.8078884642469131,
+          "pitch": -0.10981706610671438,
+          "title": "Development Area",
+          "text": "Development yards for future pits."
+        }
+      ]
+    }
+  ],
+  "name": "Elk Creek Long Branch 7-29-2021",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
